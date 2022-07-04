f2366391b42ffcf4d9b8b71b02577c52957bf97c
oyaryo <file_604@yahoo.co.jp>
2022-07-04 21:48:20 +0900

    update Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+exports.fetchTickets = functions.https.onRequest(async (request, response) => {
+  if (request.method !== "GET") {
+    response.status(400).send("リクエストタイプが不正です。");
+  }
+
+  const query = request.query.doc;
+  if (query === undefined) {
+    response.status(400).send("クエリが不正です。");
+  }
+  try {
+    const db = admin.firestore();
+    const doc = await db.collection("tickets").doc(query).get(); // eslint-disable-line
+
+    // const ticketsInfo = doc.data().email;
+    const ticketsInfo = doc.data().email;
+    response.send(ticketsInfo);
+  } catch (e) {
+    console.error(e);
+    response.status(500).send(e);
+  }
 });
